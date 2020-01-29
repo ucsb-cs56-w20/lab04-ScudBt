ed5bc9b6ae49a03ab166c05553db183e361fbb6d
ScudBt <huiyuzhang@ucsb.edu>
2020-01-28 18:28:27 -0800

    hz - fixed controller for subtract results

--- src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
@@ -9,86 +9,296 @@ package edu.ucsb.cs56.ratcalc.model;
 
 public class Rational {
 
+
+
     private int num;
+
     private int denom;
 
-    /**
-     * greatest common divisor of a and b
-     *
-     * @param a first number
-     * @param b second number
-     * @return gcd of a and b
-     */
+
+
+    /** 
+
+	greatest common divisor of a and b
+
+	@param a first number
+
+	@param b second number
+
+	@return gcd of a and b
+
+    */
+
     public static int gcd(int a, int b) {
-        if (a == 0)
-            return b;
-        else if (b == 0)
-            return a;
-        else
-            return gcd(b % a, a);
+
+	if (a==0)
+
+	    return b;
+
+	else if (b==0)
+
+	    return a;
+
+	else
+
+	    return gcd(b%a, a);
+
     }
+
+    
 
     public Rational() {
-        this.num = 1;
-        this.denom = 1;
+
+	this.num = 1;
+
+	this.denom = 1;
+
     }
+
+
 
     public Rational(int num, int denom) {
-        if (denom == 0) {
-            throw new IllegalArgumentException("denominator may not be zero");
-        }
-        this.num = num;
-        this.denom = denom;
-        if (num != 0) {
-            int gcd = Rational.gcd(num, denom);
-            this.num /= gcd;
-            this.denom /= gcd;
-        }
+
+	if (denom== 0) {
+
+	    throw new IllegalArgumentException("denominator may not be zero");
+
+	}
+
+	this.num = num;
+
+	this.denom = denom;
+
+	if (num != 0) {
+
+	    int gcd = Rational.gcd(num,denom);
+
+	    this.num /= gcd;
+
+	    this.denom /= gcd;
+
+	}
+
+
+
+    if (this.denom < 0){
+
+        this.denom *= -1;
+
+        this.num *= -1;
+
     }
+
+
+
+    }
+
+
 
     public String toString() {
-        if (denom == 1 || num == 0)
-            return "" + num;
-        return num + "/" + denom;
+
+	if (denom == 1 || num == 0)
+
+	    return "" + num;
+
+	return num + "/" + denom;
+
     }
 
-    public int getNumerator() {
-        return this.num;
-    }
 
-    public int getDenominator() {
-        return this.denom;
-    }
+
+    public int getNumerator() { return this.num; }
+
+    public int getDenominator() { return this.denom; }
+
+
 
     public Rational times(Rational r) {
-        return new Rational(this.num * r.num, this.denom * r.denom);
+
+	return new Rational(this.num * r.num,
+
+			    this.denom * r.denom);
+
     }
+
+
 
     public static Rational product(Rational a, Rational b) {
-        return new Rational(a.num * b.num, a.denom * b.denom);
+
+	return new Rational(a.num * b.num,
+
+			    a.denom * b.denom);
+
     }
 
-    public static Rational sum(Rational a, Rational b) {
-        return new Rational(); // stub
-    }
 
-    public static Rational difference(Rational a, Rational b) {
-        return new Rational(); // stub
-    }
 
-    public static Rational quotient(Rational a, Rational b) {
-        return new Rational(); // stub
-    }
-    /**
-     * For testing getters.
-     *
-     * @param args unused
+    
+
+    
+
+    /** 
+
+	For testing getters.  
+
+	@param args unused
+
      */
 
-    public static void main(String[] args) {
-        Rational r = new Rational(5, 7);
-        System.out.println("r.getNumerator()=" + r.getNumerator());
-        System.out.println("r.getDenominator()=" + r.getDenominator());
+
+
+    public static void main (String [] args) {
+
+	Rational r = new Rational(5,7);
+
+	System.out.println("r.getNumerator()=" + r.getNumerator());
+
+	System.out.println("r.getDenominator()=" + r.getDenominator());
+
     }
+
+
+
+    
+
+
+
+    public Rational simplify () {
+
+	int gcd = Rational.gcd(this.num, this.denom);
+
+	this.num /= gcd;
+
+	this.denom /= gcd;
+
+    
+
+	if (this.denom < 0){
+
+        this.denom *= -1;
+
+        this.num *= -1;
+
+    }
+
+
+
+    return this;
+
+    }
+
+
+
+    public static int lcm(int a, int b) {
+
+	return Math.abs(a * b) / gcd(a, b);
+
+    }
+
+
+
+    public Rational plus(Rational r) {
+
+	Rational a = new Rational();
+
+	a.num = this.num*r.denom + r.num*this.denom;
+
+	a.denom = this.denom*r.denom;
+
+	return a.simplify();
+
+    }
+
+
+
+    public static Rational sum(Rational a, Rational b) {
+
+	Rational r = new Rational();
+
+	r.num = a.num*b.denom + b.num*a.denom;
+
+	r.denom = a.denom*b.denom;
+
+	return r.simplify();
+
+    }
+
+
+
+    public Rational minus(Rational r) {
+
+	Rational a = new Rational();
+
+        a.num = this.num*r.denom - r.num*this.denom;
+
+        a.denom = this.denom*r.denom;
+
+        return a.simplify();	
+
+    }
+
+
+
+    public static Rational difference(Rational a, Rational b) {
+
+	Rational r = new Rational();
+
+        r.num = a.num*b.denom - b.num*a.denom;
+
+        r.denom = a.denom*b.denom;
+
+        return r.simplify();
+
+    }
+
+	
+
+    public Rational reciprocalOf() {
+
+	if (this.num == 0){
+
+        throw new ArithmeticException("denominator may not be zero");
+
+	}
+
+    int temp = this.num;
+
+	this.num = this.denom;
+
+	this.denom = temp;
+
+	return this.simplify();
+
+    }
+
+
+
+    public Rational dividedBy(Rational r) {
+
+	Rational n = r.reciprocalOf();
+
+        this.num = n.num * this.num;
+
+	this.denom = n.denom * this.denom;
+
+	return this.simplify();
+
+    }
+
+
+
+    public static Rational quotient(Rational a, Rational b) {
+
+	Rational n = product(a, b.reciprocalOf());
+
+	return n.simplify();
+
+    }
+
+
+
+
+
+	    
 
 }
--- src/test/java/edu/ucsb/cs56/ratcalc/model/RationalTest.java
+++ src/test/java/edu/ucsb/cs56/ratcalc/model/RationalTest.java
@@ -2,165 +2,654 @@ package edu.ucsb.cs56.ratcalc.model;
 
 import static org.junit.Assert.assertEquals;
 
+
+
 import org.junit.Test;
+
 import org.junit.Before;
+
+
 
 public class RationalTest {
 
-    private Rational r_5_15;
-    private Rational r_24_6;
-    private Rational r_3_7;
-    private Rational r_13_4;
-    private Rational r_20_25;
-    private Rational r_25_20;
-    private Rational r_0_1;
 
-    @Before
-    public void setUp() {
-        r_5_15 = new Rational(5, 15);
-        r_24_6 = new Rational(24, 6);
-        r_3_7 = new Rational(3, 7);
-        r_13_4 = new Rational(13, 4);
-        r_20_25 = new Rational(20, 25);
-        r_0_1 = new Rational(0, 1);
+
+	private Rational r_5_15;
+
+	private Rational r_24_6;
+
+	private Rational r_3_7;  
+
+	private Rational r_13_4;
+
+	private Rational r_20_25;
+
+	private Rational r_25_20;
+
+	private Rational r_0_1; 
+
+
+
+	@Before public void setUp() {
+
+	r_5_15 = new Rational(5,15);
+
+	r_24_6 = new Rational(24,6);
+
+	r_3_7  = new Rational(3,7);
+
+	r_13_4 = new Rational(13,4);
+
+	r_20_25 = new Rational(20,25);
+
+	r_0_1 = new Rational(0,1);
+
+	}
+
+
+
+	@Test(expected = IllegalArgumentException.class)
+
+	public void test_denom_zero() {
+
+		Rational r = new Rational(1,0);
+
+	}
+
+
+
+	@Test
+
+	public void test_getNumerator_20_25() {
+
+		assertEquals(4, r_20_25.getNumerator());
+
+	}
+
+
+
+	@Test
+
+	public void test_getNumerator_13_4() {
+
+		assertEquals(13, r_13_4.getNumerator());
+
+	}
+
+
+
+	@Test
+
+	public void test_getDenominator_20_25() {
+
+		assertEquals(5, r_20_25.getDenominator());
+
+	}
+
+
+
+	@Test
+
+	public void test_getDenominator_13_4() {
+
+		assertEquals(4, r_13_4.getDenominator());
+
+	}
+
+
+
+	@Test
+
+	public void test_toString_5_15() {
+
+		assertEquals("1/3",r_5_15.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_toString_24_6() {
+
+		assertEquals("4",r_24_6.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_toString_3_7() {
+
+		assertEquals("3/7",r_3_7.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_toString_20_25() {
+
+		assertEquals("4/5",r_20_25.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_toString_0_1() {
+
+		assertEquals("0",r_0_1.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_5_15() {
+
+		assertEquals(5, Rational.gcd(5,15));
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_15_5() {
+
+		assertEquals(5, Rational.gcd(15,5));
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_24_6() {
+
+		assertEquals(6, Rational.gcd(24,6));
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_17_5() {
+
+		assertEquals(1, Rational.gcd(17,5));
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_1_1() {
+
+		assertEquals(1, Rational.gcd(1,1));
+
+	}
+
+
+
+	@Test
+
+	public void test_gcd_20_25() {
+
+		assertEquals(5, Rational.gcd(20,25));
+
+	}
+
+
+
+	@Test
+
+	public void test_rational_m10_m5() {
+
+		Rational r = new Rational(-10,-5);
+
+		assertEquals("2",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_rational_m5_6() {
+
+		Rational r = new Rational(-5,6);
+
+		assertEquals("-5/6",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_rational_7_m8() {
+
+		Rational r = new Rational(7,-8);
+
+		assertEquals("-7/8",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_5_15_times_r_3_7() {
+
+		Rational r = r_5_15.times(r_3_7);
+
+		assertEquals("1/7",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_3_7_times_r_13_4() {
+
+		Rational r = r_3_7.times(r_13_4);
+
+		assertEquals("39/28",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_m3_1_times_1_m3() {
+
+		Rational r_m3_1 = new Rational(-3,1);
+
+		Rational r_1_m3 = new Rational(1,-3);
+
+		Rational r = r_m3_1.times(r_1_m3);
+
+		assertEquals("1",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_product_of_r_5_15_and_r_3_7() {
+
+		Rational r = Rational.product(r_5_15,r_3_7);
+
+		assertEquals("1/7",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_product_of_r_3_7_and_r_13_4() {
+
+		Rational r = Rational.product(r_3_7,r_13_4);
+
+		assertEquals("39/28",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_product_of_r_m3_1_and_1_m3() {
+
+		Rational r_m3_1 = new Rational(-3,1);
+
+		Rational r_1_m3 = new Rational(1,-3);
+
+		Rational r = Rational.product(r_m3_1,r_1_m3);
+
+		assertEquals("1",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_lcm_9_18() {
+
+		assertEquals(18, Rational.lcm(9,18));
+
+	}
+
+
+
+	@Test
+
+	public void test_lcm_9_0() {
+
+		assertEquals(0, Rational.lcm(9,0));
+
+	}
+
+
+
+	@Test
+
+	public void test_lcm_3_18() {
+
+		assertEquals(18, Rational.lcm(3,18));
+
+	}
+
+
+
+	@Test
+
+	public void test_r_24_6_plus_r_5_15() {
+
+		Rational r = r_24_6.plus(r_5_15);
+
+		assertEquals("13/3", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_0_1_plus_r_3_7() {
+
+		Rational r = r_0_1.plus(r_3_7);
+
+		assertEquals("3/7", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_n3_5_plus_r_n3_7() {
+
+		Rational a = new Rational(-3,5); 
+
+		Rational b = new Rational(-3,7);
+
+		Rational r = a.plus(b);
+
+		assertEquals("-36/35", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_24_6_sum_r_5_15() {
+
+		Rational r = Rational.sum(r_24_6, r_5_15);
+
+		assertEquals("13/3", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_0_1_sum_r_3_7() {
+
+		Rational r = Rational.sum(r_0_1, r_3_7);
+
+		assertEquals("3/7", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_n3_5_sum_r_n3_7() {
+
+		Rational a = new Rational(-3,5); 
+
+		Rational b = new Rational(-3,7);
+
+		Rational r = Rational.sum(a, b);
+
+		assertEquals("-36/35", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_24_6_minus_r_5_15() {
+
+		Rational r = r_24_6.minus(r_5_15);
+
+		assertEquals("11/3", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_0_1_minus_r_3_7() {
+
+		Rational r = r_0_1.minus(r_3_7);
+
+		assertEquals("-3/7", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_n3_5_minus_r_n3_7() {
+
+		Rational a = new Rational(-3,5); 
+
+		Rational b = new Rational(-3,7);
+
+		Rational r = a.minus(b);
+
+		assertEquals("-6/35", r.toString());
+
+	}
+
+
+
+
+
+	@Test
+
+	public void test_r_24_6_difference_r_5_15() {
+
+		Rational r = Rational.difference(r_24_6, r_5_15);
+
+		assertEquals("11/3", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_0_1_difference_r_3_7() {
+
+		Rational r = Rational.difference(r_0_1, r_3_7);
+
+		assertEquals("-3/7", r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_n3_5_difference_r_n3_7() {
+
+		Rational a = new Rational(-3,5); 
+
+		Rational b = new Rational(-3,7);
+
+		Rational r = Rational.difference(a, b);
+
+		assertEquals("-6/35", r.toString());
+
+	}
+
+
+
+
+
+	@Test(expected = ArithmeticException.class)
+
+	public void test_reciprocalOf_zero() {
+
+		Rational r = r_0_1.reciprocalOf();
+
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_denom_zero() {
-        Rational r = new Rational(1, 0);
-    }
 
-    @Test
-    public void test_getNumerator_20_25() {
-        assertEquals(4, r_20_25.getNumerator());
-    }
 
-    @Test
-    public void test_getNumerator_13_4() {
-        assertEquals(13, r_13_4.getNumerator());
-    }
 
-    @Test
-    public void test_getDenominator_20_25() {
-        assertEquals(5, r_20_25.getDenominator());
-    }
 
-    @Test
-    public void test_getDenominator_13_4() {
-        assertEquals(4, r_13_4.getDenominator());
-    }
+	@Test 
 
-    @Test
-    public void test_toString_5_15() {
-        assertEquals("1/3", r_5_15.toString());
-    }
+	public void test_r_n3_5_reciprocalOf(){
 
-    @Test
-    public void test_toString_24_6() {
-        assertEquals("4", r_24_6.toString());
-    }
+		Rational r = new Rational(-3,5); 
 
-    @Test
-    public void test_toString_3_7() {
-        assertEquals("3/7", r_3_7.toString());
-    }
+		r = r.reciprocalOf();
 
-    @Test
-    public void test_toString_20_25() {
-        assertEquals("4/5", r_20_25.toString());
-    }
+        assertEquals("-5/3", r.toString());
 
-    @Test
-    public void test_toString_0_1() {
-        assertEquals("0", r_0_1.toString());
-    }
+	}
 
-    @Test
-    public void test_gcd_5_15() {
-        assertEquals(5, Rational.gcd(5, 15));
-    }
 
-    @Test
-    public void test_gcd_15_5() {
-        assertEquals(5, Rational.gcd(15, 5));
-    }
 
-    @Test
-    public void test_gcd_24_6() {
-        assertEquals(6, Rational.gcd(24, 6));
-    }
+	@Test 
 
-    @Test
-    public void test_gcd_17_5() {
-        assertEquals(1, Rational.gcd(17, 5));
-    }
+	public void test_r_3_7_reciprocalOf(){
 
-    @Test
-    public void test_gcd_1_1() {
-        assertEquals(1, Rational.gcd(1, 1));
-    }
+        Rational r = r_3_7.reciprocalOf();
 
-    @Test
-    public void test_gcd_20_25() {
-        assertEquals(5, Rational.gcd(20, 25));
-    }
+		assertEquals("7/3", r.toString());
 
-    @Test
-    public void test_rational_m10_m5() {
-        Rational r = new Rational(-10, -5);
-        assertEquals("2", r.toString());
-    }
+	}
 
-    @Test
-    public void test_rational_m5_6() {
-        Rational r = new Rational(-5, 6);
-        assertEquals("-5/6", r.toString());
-    }
 
-    @Test
-    public void test_rational_7_m8() {
-        Rational r = new Rational(7, -8);
-        assertEquals("-7/8", r.toString());
-    }
 
-    @Test
-    public void test_r_5_15_times_r_3_7() {
-        Rational r = r_5_15.times(r_3_7);
-        assertEquals("1/7", r.toString());
-    }
+	@Test
 
-    @Test
-    public void test_r_3_7_times_r_13_4() {
-        Rational r = r_3_7.times(r_13_4);
-        assertEquals("39/28", r.toString());
-    }
+	public void test_r_5_15_dividedBy_r_3_7() {
 
-    @Test
-    public void test_r_m3_1_times_1_m3() {
-        Rational r_m3_1 = new Rational(-3, 1);
-        Rational r_1_m3 = new Rational(1, -3);
-        Rational r = r_m3_1.times(r_1_m3);
-        assertEquals("1", r.toString());
-    }
+		Rational r = r_5_15.dividedBy(r_3_7);
 
-    @Test
-    public void test_product_of_r_5_15_and_r_3_7() {
-        Rational r = Rational.product(r_5_15, r_3_7);
-        assertEquals("1/7", r.toString());
-    }
+		assertEquals("7/9",r.toString()); // fix simplification 
 
-    @Test
-    public void test_product_of_r_3_7_and_r_13_4() {
-        Rational r = Rational.product(r_3_7, r_13_4);
-        assertEquals("39/28", r.toString());
-    }
+	}
 
-    @Test
-    public void test_product_of_r_m3_1_and_1_m3() {
-        Rational r_m3_1 = new Rational(-3, 1);
-        Rational r_1_m3 = new Rational(1, -3);
-        Rational r = Rational.product(r_m3_1, r_1_m3);
-        assertEquals("1", r.toString());
-    }
+
+
+	@Test
+
+	public void test_r_3_7_dividedBy_r_13_4() {
+
+		Rational r = r_3_7.dividedBy(r_13_4);
+
+		assertEquals("12/91",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_r_m3_1_dividedBy_1_m3() {
+
+		Rational r_m3_1 = new Rational(-3,1);
+
+		Rational r_1_m3 = new Rational(1,-3);
+
+		Rational r = r_m3_1.dividedBy(r_1_m3);
+
+		assertEquals("9",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_quotient_of_r_5_15_and_r_3_7() {
+
+		Rational r = Rational.quotient(r_5_15,r_3_7);
+
+		assertEquals("7/9",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_quotient_of_r_3_7_and_r_13_4() {
+
+		Rational r = Rational.quotient(r_3_7,r_13_4);
+
+		assertEquals("12/91",r.toString());
+
+	}
+
+
+
+	@Test
+
+	public void test_quotient_of_r_m3_1_and_1_m3() {
+
+		Rational r_m3_1 = new Rational(-3,1);
+
+		Rational r_1_m3 = new Rational(1,-3);
+
+		Rational r = Rational.quotient(r_m3_1,r_1_m3);
+
+		assertEquals("9",r.toString());
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
